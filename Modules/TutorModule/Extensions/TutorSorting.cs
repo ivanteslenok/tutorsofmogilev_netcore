9ceeeca3e70672c7fab1f3d076f9385ef260ad21
Ivan Teslenok <ivanteslenok@mail.ru>
2018-11-24 21:16:34 +0300

    дополнена сортировка сущности Tutor

--- Modules/TutorModule/Extensions/TutorSorting.cs
+++ Modules/TutorModule/Extensions/TutorSorting.cs
@@ -24,6 +24,17 @@ namespace Modules.TutorModule.Extensions
                             break;
                     }
                     break;
+                case "FIRSTNAME":
+                    switch (direction)
+                    {
+                        case "ASC":
+                            query = query.OrderBy(t => t.FirstName);
+                            break;
+                        case "DESC":
+                            query = query.OrderByDescending(t => t.FirstName);
+                            break;
+                    }
+                    break;
                 case "LASTNAME":
                     switch (direction)
                     {
@@ -32,6 +43,105 @@ namespace Modules.TutorModule.Extensions
                             break;
                         case "DESC":
                             query = query.OrderByDescending(t => t.LastName);
+                            break;
+                    }
+                    break;
+                case "PATRONYMIC":
+                    switch (direction)
+                    {
+                        case "ASC":
+                            query = query.OrderBy(t => t.Patronymic);
+                            break;
+                        case "DESC":
+                            query = query.OrderByDescending(t => t.Patronymic);
+                            break;
+                    }
+                    break;
+                case "DESCRIPTION":
+                    switch (direction)
+                    {
+                        case "ASC":
+                            query = query.OrderBy(t => t.Description);
+                            break;
+                        case "DESC":
+                            query = query.OrderByDescending(t => t.Description);
+                            break;
+                    }
+                    break;
+                case "EDUCATION":
+                    switch (direction)
+                    {
+                        case "ASC":
+                            query = query.OrderBy(t => t.Education);
+                            break;
+                        case "DESC":
+                            query = query.OrderByDescending(t => t.Education);
+                            break;
+                    }
+                    break;
+                case "JOB":
+                    switch (direction)
+                    {
+                        case "ASC":
+                            query = query.OrderBy(t => t.Job);
+                            break;
+                        case "DESC":
+                            query = query.OrderByDescending(t => t.Job);
+                            break;
+                    }
+                    break;
+                case "ADDRESS":
+                    switch (direction)
+                    {
+                        case "ASC":
+                            query = query.OrderBy(t => t.Address);
+                            break;
+                        case "DESC":
+                            query = query.OrderByDescending(t => t.Address);
+                            break;
+                    }
+                    break;
+                case "EXPERIENCE":
+                    switch (direction)
+                    {
+                        case "ASC":
+                            query = query.OrderBy(t => t.Experience);
+                            break;
+                        case "DESC":
+                            query = query.OrderByDescending(t => t.Experience);
+                            break;
+                    }
+                    break;
+                case "COST":
+                    switch (direction)
+                    {
+                        case "ASC":
+                            query = query.OrderBy(t => t.Cost);
+                            break;
+                        case "DESC":
+                            query = query.OrderByDescending(t => t.Cost);
+                            break;
+                    }
+                    break;
+                case "ISVISIBLE":
+                    switch (direction)
+                    {
+                        case "ASC":
+                            query = query.OrderBy(t => t.IsVisible);
+                            break;
+                        case "DESC":
+                            query = query.OrderByDescending(t => t.IsVisible);
+                            break;
+                    }
+                    break;
+                case "DISTRICT":
+                    switch (direction)
+                    {
+                        case "ASC":
+                            query = query.OrderBy(t => t.District.Name);
+                            break;
+                        case "DESC":
+                            query = query.OrderByDescending(t => t.District.Name);
                             break;
                     }
                     break;
