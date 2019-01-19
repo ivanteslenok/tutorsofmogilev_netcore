9614c7d7161afd074c23dc45025d7a77427a7a92
Ivan Teslenok <ivanteslenok@mail.ru>
2019-01-20 00:21:40 +0300

    added gulp for sass compile and css minification

---
+++ TutorsOfMogilev_NetCore/wwwroot/gulpfile.js
@@ -0,0 +1,18 @@
+﻿var gulp = require('gulp');
+var gulpSass = require('gulp-sass');
+var gulpCssNano = require('gulp-cssnano');
+
+var sassSrc = './styles/*.scss',
+    cssDest = './styles/';
+
+gulp.task('sass', function (done) {
+    gulp.src(sassSrc)
+        .pipe(gulpSass())
+        .pipe(gulpCssNano())
+        .pipe(gulp.dest(cssDest));
+    done();
+});
+
+gulp.task('watch', function () {
+    gulp.watch(sassSrc, gulp.series('sass'));
+});
