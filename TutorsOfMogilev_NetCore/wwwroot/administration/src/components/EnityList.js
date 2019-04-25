58e9224d7910fc367a8c5133ea307902ee43d31a
Ivan Tsiaslenak <Ivan.Tsiaslenak@artezio.com>
2019-04-25 14:23:51 +0300

    rewrite EntityList component with hooks

--- TutorsOfMogilev_NetCore/wwwroot/administration/src/components/EnityList.js
+++ TutorsOfMogilev_NetCore/wwwroot/administration/src/components/EnityList.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import EditableList from './EditableList';
 import DeleteConfirmDialog from './DeleteConfirmDialog';
 import DialogWithInput from './DialogWithInput';
@@ -7,104 +7,104 @@ import CreateBtn from './CreateBtn';
 import Loading from './Loading';
 import Paper from '@material-ui/core/Paper';
 
-export default class EnityList extends Component {
-  state = {
-    changingItemId: null,
-    isEdit: false
-  };
+const EnityList = props => {
+  const [changingItemId, setChangingItemId] = useState(null);
+  const [isEdit, setIsEdit] = useState(false);
 
-  componentDidMount() {
-    const { loading, loaded, loadData } = this.props;
+  useEffect(() => {
+    const { loading, loaded, loadData } = props;
     if (!loaded && !loading) loadData();
-  }
+  });
 
-  handleEditStart = id => {
-    this.setState({ changingItemId: id, isEdit: true });
-    this.props.openInputDialog();
+  const handleEditStart = id => {
+    setChangingItemId(id);
+    setIsEdit(true);
+    props.openInputDialog();
   };
 
-  handleDeleteStart = (id, text) => {
-    this.setState({ changingItemId: id, isEdit: false });
-    this.props.openDeleteDialog();
+  const handleDeleteStart = id => {
+    setChangingItemId(id);
+    setIsEdit(false);
+    props.openDeleteDialog();
   };
 
-  handleDeleteConfirm = () => {
-    this.props.remove(this.state.changingItemId);
-    this.props.closeDeleteDialog();
+  const handleDeleteConfirm = () => {
+    props.remove(changingItemId);
+    props.closeDeleteDialog();
   };
 
-  handleAccept = value => {
-    const { update, create } = this.props;
-    const { changingItemId, isEdit } = this.state;
+  const handleAccept = value => {
+    const { update, create } = props;
 
     isEdit ? update(changingItemId, { name: value }) : create({ name: value });
 
-    this.setState({ changingItemId: null, isEdit: false });
+    setChangingItemId(null);
+    setIsEdit(false);
   };
 
-  handleEditClose = () => {
-    this.setState({ changingItemId: null, isEdit: false });
-    this.props.closeInputDialog();
+  const handleEditClose = () => {
+    setChangingItemId(null);
+    setIsEdit(false);
+    props.closeInputDialog();
   };
 
-  render() {
-    const {
-      items,
-      loading,
-      deleteDialogOpen,
-      closeDeleteDialog,
-      withInputDialogOpen,
-      openInputDialog,
-      closeInputDialog
-    } = this.props;
+  const {
+    items,
+    loading,
+    deleteDialogOpen,
+    closeDeleteDialog,
+    withInputDialogOpen,
+    openInputDialog,
+    closeInputDialog
+  } = props;
 
-    const { changingItemId, isEdit } = this.state;
+  const description = 'Введите название.';
 
-    const description = 'Введите название.';
+  const deleteItem =
+    changingItemId !== null && _.find(items, { id: changingItemId });
+  const deleteText = deleteItem && deleteItem.name;
 
-    const deleteItem = changingItemId !== null && _.find(items, { id: changingItemId });
-    const deleteText = deleteItem && deleteItem.name;
+  const content = loading ? (
+    <Loading />
+  ) : (
+    <>
+      <EditableList
+        items={items}
+        handleItemEdit={handleEditStart}
+        handleItemDelete={handleDeleteStart}
+      />
+      <div>
+        <CreateBtn handleClick={openInputDialog} />
+      </div>
+      <DeleteConfirmDialog
+        isOpen={deleteDialogOpen}
+        text={deleteText}
+        handleClose={closeDeleteDialog}
+        handleConfirm={handleDeleteConfirm}
+      />
+      <DialogWithInput
+        title={isEdit ? 'Редактирование' : 'Создание'}
+        description={description}
+        isOpen={withInputDialogOpen}
+        handleClose={isEdit ? handleEditClose : closeInputDialog}
+        inputValue={isEdit ? _.find(items, { id: changingItemId }).name : ''}
+        handleAccept={handleAccept}
+      />
+    </>
+  );
 
-    const content = loading ? (
-      <Loading />
-    ) : (
-      <>
-        <EditableList
-          items={items}
-          handleItemEdit={this.handleEditStart}
-          handleItemDelete={this.handleDeleteStart}
-        />
-        <div>
-          <CreateBtn handleClick={openInputDialog} />
-        </div>
-        <DeleteConfirmDialog
-          isOpen={deleteDialogOpen}
-          text={deleteText}
-          handleClose={closeDeleteDialog}
-          handleConfirm={this.handleDeleteConfirm}
-        />
-        <DialogWithInput
-          title={isEdit ? 'Редактирование' : 'Создание'}
-          description={description}
-          isOpen={withInputDialogOpen}
-          handleClose={isEdit ? this.handleEditClose : closeInputDialog}
-          inputValue={isEdit ? _.find(items, { id: changingItemId }).name : ''}
-          handleAccept={this.handleAccept}
-        />
-      </>
-    );
+  return (
+    <Paper
+      style={{
+        width: '50%',
+        minHeight: '100px',
+        margin: '0 auto',
+        position: 'relative'
+      }}
+    >
+      {content}
+    </Paper>
+  );
+};
 
-    return (
-      <Paper
-        style={{
-          width: '50%',
-          minHeight: '100px',
-          margin: '0 auto',
-          position: 'relative'
-        }}
-      >
-        {content}
-      </Paper>
-    );
-  }
-}
+export default EnityList;
